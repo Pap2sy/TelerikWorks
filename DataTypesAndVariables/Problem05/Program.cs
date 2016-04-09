53df36ad9c12a0892c34e1a03df4b77dc0c8c3fb
Pap2sy <Pap2sy@gmail.com>
2016-04-09 21:00:17 +0300

    Added tasks 5 and 6

---
+++ DataTypesAndVariables/Problem05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem05
+{
+    class Program
+    {
+//        Problem 5. Boolean Variable
+
+//Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender.
+//Print it on the console.
+        static void Main(string[] args)
+        {
+            string firstName = "Pesho";
+            string lastName = "Ivanov";
+            string gender = "Male";
+            bool isFemale;
+
+            if (gender == "Male")
+            {
+                isFemale = false;
+
+            }
+            else
+            {
+                isFemale = true;
+            }
+            Console.WriteLine("isFemale = " + isFemale);
+        }
+    }
+}
---
+++ DataTypesAndVariables/Problem06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem06
+{
+    class Program
+    {
+//        Problem 6. Strings and Objects
+
+//Declare two string variables and assign them with Hello and World.
+//Declare an object variable and assign it with the concatenation of the first two variables 
+//    (mind adding an interval between).
+//Declare a third string variable and initialize it with the value of the object variable
+//    (you should perform type casting)
+        static void Main(string[] args)
+        {
+            string message1 = "Hello";
+            string message2 = " World";
+
+            object sumMsg = message1 + message2;
+            Console.WriteLine(sumMsg);
+            string msg3 = (string)sumMsg;
+            Console.WriteLine(msg3);
+        }
+    }
+}
