4a726f7a642b511148e2ad4532523bf31f1f0a5f
Pap2sy <Pap2sy@gmail.com>
2017-01-29 23:21:43 +0200

    Added problem01 and 02 at Operators and Expressions homework

---
+++ Operators and Expressions/Problem01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem01
+{
+//    Odd or Even Integers
+
+//Write an expression that checks if given integer is odd or even.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a nuber");
+            int number = int.Parse(Console.ReadLine());
+            bool checker;
+            if (number % 2 == 0) // checks if is even
+            {
+                checker = true;
+                Console.WriteLine(checker + " The number is even");
+            }
+            else
+            {
+                checker = false;
+                Console.WriteLine(checker + " The number is Odd");
+            }
+        }
+    }
+}
---
+++ Operators and Expressions/Problem02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem02
+{
+//    Gravitation on the Moon
+
+//The gravitational field of the Moon is approximately 17% of that on the Earth.
+//Write a program that calculates the weight of a man on the moon by a given weight on the Earth.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your weight on Earth" );
+            double eartWeight = double.Parse(Console.ReadLine());
+            double moonWeight = eartWeight * 0.17;
+            Console.WriteLine( "Your weight on moon will be {0}",moonWeight);
+
+        }
+    }
+}
