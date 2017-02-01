dda11c3089f15170dded33d0449f077d8ed55bc0
Pap2sy <Pap2sy@gmail.com>
2017-02-01 23:05:32 +0200

    added problem 3 and 4

---
+++ Operators and Expressions/Problem03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem03
+{
+//    Divide by 7 and 5
+
+//Write a Boolean expression that checks for given integer if it can be divided (without remainder) by 7 and 5 at the same time
+    class Program
+    {
+        static void Main()
+        {
+            int number;
+            number = int.Parse(Console.ReadLine());
+            bool checker = false;
+            if (number % 5 == 0 && number % 7 == 0)
+            {
+                checker = true;
+            }
+            else
+            {
+                checker = false;
+            }
+            Console.WriteLine((checker).ToString().ToLower());
+
+            
+               
+        }
+    }
+}
---
+++ Operators and Expressions/Problem04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem04
+{
+    //    Rectangles
+
+    //Write an expression that calculates rectangle’s perimeter and area by given width and height.
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("enter width");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter height");
+            double height = double.Parse(Console.ReadLine());
+            double perimeter = (width * 2) + (height * 2);
+            double area = width * height;
+            Console.WriteLine("perimeter is:" +perimeter);
+            Console.WriteLine("area is :" + area);
+
+
+        }
+    }
+}
